cae1ce1bf2d629a9dab2459faf39b42c38d12748
Hana Kessler <58595147+hanakslr@users.noreply.github.com>
2025-02-10 09:17:28 -0500

    segment railways by intersections (#1)

--- src/main.rs
+++ src/main.rs
@@ -1,167 +1,174 @@
-use std::collections::HashMap;
-
+use crate::reader::Filter;
 use osmpbf::{Element, ElementReader};
+use serde::{Deserialize, Serialize};
+use std::collections::{HashMap, HashSet};
 
+const FILE_NAME: &str = "./osm_data/us-northeast-latest.osm.pbf";
+
+mod reader;
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct OsmRailway {
     // Represent the railway from OSM - moderately untransformed, without any splitting done.
     name: String,
 
     way_id: i64,
 
-    nodes: Vec<i64>, // A list of the node_ids that make up this way.
+    node_ids: Vec<i64>, // A list of the node_ids that make up this way.
 }
 
-struct OsmNode {
-    // Represents a single OsmNode. Keyed by node_id - these ids correspond to the values of OsmRailway.nodes.
-    // Our library can exapnd the Way out into lat/longs directly, but it will be more efficient to split Railways into
-    // segments based on intersecting node_id than lat/long.
-    node_id: i64,
-    latitude: f64,
-    longitude: f64,
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RailwaySegment {
+    name: String,
+    way_id: i64,
+    node_ids: Vec<i64>,
 }
 
-fn collect_all_railways() {
-    let reader = ElementReader::from_path("./osm_data/us-northeast-latest.osm.pbf")
-        .expect("Could not load data");
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct LatLong(f64, f64);
 
-    // Iterate over every element, find the ways - create a Vec of OsmRailway.
-    // This particular reader requires that the map_op return the same type as the reduce_op - instead of a reduce function that
-    // takes a previous value. This makes sense given the parallelization I think, but it mean that I make these intermediary, single
-    // element vecs.
-    let (railways, nodes) = reader
-        .par_map_reduce(
-            |element| match element {
-                Element::Way(way) => {
-                    match way
-                        .tags()
-                        .any(|(key, value)| key == "railway" && value == "rail")
-                    {
-                        true => {
-                            let way_name = match way.tags().find(|(key, _)| *key == "name") {
-                                Some(res) => res.1.to_string(),
-                                _ => way.id().to_string(),
-                            };
+impl OsmRailway {
+    pub fn from_osm_way(way: &osmpbf::Way) -> OsmRailway {
+        let way_name = match way.tags().find(|(key, _)| *key == "name") {
+            Some(res) => res.1.to_string(),
+            _ => way.id().to_string(),
+        };
 
-                            let railway = OsmRailway {
-                                name: way_name,
-                                way_id: way.id(),
-                                nodes: way.raw_refs().to_vec(),
-                            };
-
-                            (Some(vec![railway]), None)
-                        }
-                        false => (None, None),
-                    }
-                }
-                Element::DenseNode(node) => (
-                    None,
-                    Some(vec![OsmNode {
-                        node_id: node.id(),
-                        latitude: node.lat(),
-                        longitude: node.lon(),
-                    }]),
-                ),
-                _ => (None, None),
-            },
-            || (None, None),
-            |(a_railways, a_nodes), (b_railways, b_nodes)| {
-                // Get our rails and nodes from a
-                let railways = match (a_railways, b_railways) {
-                    (res @ Some(_), None) => res,
-                    (None, res @ Some(_)) => res,
-                    (Some(r1), Some(r2)) => Some(r1.into_iter().chain(r2).collect()),
-                    _ => None,
-                };
-
-                let nodes = match (a_nodes, b_nodes) {
-                    (res @ Some(_), None) => res,
-                    (None, res @ Some(_)) => res,
-                    (Some(r1), Some(r2)) => Some(r1.into_iter().chain(r2).collect()),
-                    _ => None,
-                };
-
-                (railways, nodes)
-            },
-        )
-        .expect("Error loading OsmRailways");
-
-    match railways {
-        None => println!("None found"),
-        Some(r) => {
-            let num_ways = r.len();
-            println!("Number of railways {num_ways}");
+        OsmRailway {
+            name: way_name,
+            way_id: way.id(),
+            node_ids: way.raw_refs().to_owned(),
         }
     }
 
-    match nodes {
-        None => println!("No nodes found"),
-        Some(r) => {
-            let num_nodes = r.len();
-            println!("Number of nodes {num_nodes}")
-        }
-    }
-}
-
-/// Given a vec of OsmRailways, we want to reduce to a graph of railway segments that
-/// are broken up at interesections from other segments. That each segments start_node and end_node
-/// should either be a terminals or contain an intersection with another segment
-fn segment_railways(railways: Vec<OsmRailway>) -> Vec<OsmRailway> {
-    // Make a mapping of node_id to the OsmRailway and index that it is first seen at
-    // If a node_id already exists in the mapping - split that Railway into 2 segments, as well as the new one
-    let mut split_railways = Vec::new();
-    let previous_nodes = HashMap::<i64, (&OsmRailway, usize)>::new();
-
-    for railway in railways {
-        // iterate over the nodes - check to see if the node_id is in the hashmap
-        for (index, n) in railway.nodes.iter().enumerate() {
-            if previous_nodes.contains_key(n) {
-                // This node exists in another segment, which means we need to break up that segment AND
-                // this is the end of this segment.
-                //let new_slice = railway.nodes.split_off(index);
-                split_railways.push(OsmRailway {
-                    name: railway.name.clone(),
-                    way_id: railway.way_id,
-                    nodes: Vec::new(),
-                })
-            } else {
-                // We haven't seen this node before - add it to the mapping so we know that it has been used
+    /// Helper function to get a count of the number of times nodes are used in a vec of `OsmRailway`s.
+    pub fn get_used_node_counts(railways: &Vec<OsmRailway>) -> HashMap<i64, i64> {
+        // Iterate over all of the railways and find make a hashap of nodes and the number
+        // of times they are used.
+        let mut node_use_count = HashMap::new();
+        for railway in railways.iter() {
+            for node_id in railway.node_ids.iter() {
+                let count = node_use_count.entry(*node_id).or_insert(0);
+                *count += 1;
             }
         }
+
+        node_use_count
     }
 
-    Vec::new()
+    pub fn to_railway_segments(self, intersections: &HashSet<i64>) -> Vec<RailwaySegment> {
+        // Keep track of the index as we iterate through the nodes. We never split on the
+        // first node or we end up with a segment of length 1 which is useless.
+        let mut index = 0;
+
+        // Additionally, keep track of the last node in the previous split. We add it to the
+        // beginning of the next split so that we don't lose that edge.
+        let mut previous_node = None;
+        let railway_segments: Vec<RailwaySegment> = self
+            .node_ids
+            .split_inclusive(|n| {
+                let should_split = index != 0 && intersections.contains(n);
+                index += 1;
+                should_split
+            })
+            .map(|nodes| {
+                // If there is a previous_node, add it at the beginning of the list
+                let node_ids: Vec<_> = if let Some(previous_node) = previous_node {
+                    let mut vec = Vec::with_capacity(nodes.len() + 1); // Reserve space for one more node
+                    vec.push(previous_node);
+                    vec.extend_from_slice(nodes); // Extend with nodes
+                    vec
+                } else {
+                    nodes.to_vec() // No previous_node, just copy the slice to Vec
+                };
+
+                previous_node = nodes.last().cloned(); // Clone last element for future use
+
+                RailwaySegment {
+                    name: self.name.clone(),
+                    way_id: self.way_id,
+                    node_ids,
+                }
+            })
+            .collect();
+
+        railway_segments
+    }
 }
 
-fn count_all_railways() {
-    // This file is downloaded from Geofabrik but is 1.5gb so it is not in source control
-    let reader = ElementReader::from_path("./osm_data/us-northeast-latest.osm.pbf")
-        .expect("Couldn't load data.");
+fn collect_nodes(node_ids: HashSet<i64>) -> HashMap<i64, LatLong> {
+    let reader = ElementReader::from_path(FILE_NAME).unwrap();
 
-    // Count the railways
-    let ways = reader
-        .par_map_reduce(
-            |element| match element {
-                Element::Way(way) => {
-                    match way
-                        .tags()
-                        .any(|(key, value)| key == "railway" && value == "rail")
-                    {
-                        true => 1,
-                        _ => 0,
+    let node_coords =
+        reader
+            .par_map_reduce(
+                |element| match element {
+                    Element::DenseNode(node) if node_ids.contains(&node.id()) => Some(
+                        HashMap::from([(node.id(), LatLong(node.lat(), node.lon()))]),
+                    ),
+                    _ => None,
+                },
+                || None,
+                |a, b| match (a, b) {
+                    (res @ Some(_), None) => res,
+                    (None, res @ Some(_)) => res,
+                    (Some(mut map1), Some(map2)) => {
+                        map1.extend(map2);
+                        Some(map1)
                     }
-                }
-                _ => 0,
-            },
-            || 0_u64,     // Zero is the identity value for addition
-            |a, b| a + b, // Sum the partial results
-        )
-        .expect("Error counting.");
+                    _ => None,
+                },
+            )
+            .expect("Error parsing nodes.")
+            .expect("No nodes found.");
 
-    println!("Number of ways: {ways}");
+    let count = node_coords.len();
+    println!("Number of nodes {count}");
+
+    node_coords
+}
+
+/// Read an OSM file and parse out all of the railways.
+fn collect_all_railways() -> Vec<OsmRailway> {
+    let railways =
+        ElementReader::<std::fs::File>::collect_filtered(FILE_NAME, |element| match element {
+            Element::Way(way) if way.tags().any(|(k, v)| k == "railway" && v == "rail") => {
+                Some(OsmRailway::from_osm_way(&way))
+            }
+            _ => None,
+        })
+        .expect("Error collecting filtered elements");
+
+    let num_ways = railways.len();
+    println!("Number of railways {num_ways}");
+
+    railways
+}
+
+fn segment_railways(railways: Vec<OsmRailway>) -> Vec<RailwaySegment> {
+    let node_counts = OsmRailway::get_used_node_counts(&railways);
+
+    let intersections = node_counts
+        .iter()
+        .filter(|(_, count)| **count > 1)
+        .map(|(node_id, _)| *node_id)
+        .collect();
+
+    let railway_segments: Vec<RailwaySegment> = railways
+        .into_iter()
+        .flat_map(|railway| railway.to_railway_segments(&intersections))
+        .collect();
+
+    let num_segments = railway_segments.len();
+
+    println!("Number of segments {num_segments}");
+
+    railway_segments
 }
 
 fn main() {
-    collect_all_railways();
+    let railways = collect_all_railways();
+    segment_railways(railways);
 }
 
 #[test]
@@ -171,25 +178,21 @@ fn test_segment_railways() {
         OsmRailway {
             name: "way 1".to_string(),
             way_id: 1,
-            nodes: Vec::from([1, 2, 3, 6, 8, 9]),
+            node_ids: Vec::from([1, 2, 3, 6, 8, 9]),
         },
         OsmRailway {
             name: "way 2".to_string(),
             way_id: 2,
-            nodes: [2, 3].to_vec(),
+            node_ids: [2, 3].to_vec(),
         },
         OsmRailway {
             name: "way 3".to_string(),
             way_id: 3,
-            nodes: [4, 5, 6, 7].to_vec(),
+            node_ids: [4, 5, 6, 7].to_vec(),
         },
     ]);
 
     let segmented_railways = segment_railways(railways);
 
-    assert_eq!(6, segmented_railways.len());
-
-    // Add a check for the nodes.
-    // Should have
-    // [1,2], [2,3], [2,6], [4,5,6], [6,7], [6,8,9]
+    assert_eq!(7, segmented_railways.len(), "{:?}", segmented_railways);
 }
